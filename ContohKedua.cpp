0c06ed898026a8356ac0966377d783e0e294edbd
ZhilPro27 <zhilalkrisna@gmail.com>
2024-03-04 17:51:28 +0700

    Fungsi random dan menampilkan bilangan

--- ContohKedua.cpp
+++ ContohKedua.cpp
@@ -4,4 +4,7 @@ using namespace std;
 int main(){
     int nBilangan;
     string cStatus;
+    srand(time(0));
+    nBilangan = rand() % 10;
+    cout << "Bilangannya adalah = " << nBilangan << endl;
 }
