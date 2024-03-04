caedf86679a633e15bfd87502ab0bbf562bd12fd
ZhilPro27 <zhilalkrisna@gmail.com>
2024-03-04 17:55:43 +0700

    Nested IF statement

--- ContohKetiga.cpp
+++ ContohKetiga.cpp
@@ -9,4 +9,13 @@ int main(){
     nBilanganB = rand() % 10;
     cout << "Bilangan A adalah = " << nBilanganA << endl;
     cout << "Bilangan B adalah = " << nBilanganB << endl;
+    if (nBilanganA == nBilanganB){
+        cStatus = "Kedua bilangan nilainya sama";
+    }
+    else if (nBilanganA > nBilanganB){
+        cStatus = "Bilangan A lebih besar";
+    }
+    else {
+        cStatus = "Bilangan B lebih besar";
+    }
 }
