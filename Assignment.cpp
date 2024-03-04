0b8eb9df0e15a8445128fbc157dc36d1ccca8978
ZhilPro27 <zhilalkrisna@gmail.com>
2024-03-04 17:59:38 +0700

    Soal

---
+++ Assignment.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //siswa mempunyai 2 nilai yaitu matematika dan fisika
+    //kemudian dihitung reratanya, jika rerata lebih dari 60
+    //maka dinyatakan lulus, atau setidaknya nilai matematika
+    //lebih dari 70 dan jika tidak keduanya maka
+    //dinyatakan tidak lulus
+}
